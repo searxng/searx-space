595dda60b6c20d4d74a7b438afa8be940258794b
Dalf <alex@al-f.net>
2020-01-07 11:26:49 +0100

    [fix] commit 4b597b675cc6c4579805f17df4c7489652102333
    - The link regex didn't accepted space in the label (LINK_RE)
    - Minor adjustments.

--- searxstats/source/__init__.py
+++ searxstats/source/__init__.py
@@ -1,1 +1,3 @@
 from .searx_docs import SEARX_INSTANCES_URL, get_instance_urls
+
+__all__ = ['SEARX_INSTANCES_URL', 'get_instance_urls']
--- searxstats/source/searx_docs.py
+++ searxstats/source/searx_docs.py
@@ -6,15 +6,15 @@ from searxstats.common.http import new_client, get_host
 SEARX_INSTANCES_URL = 'https://raw.githubusercontent.com/asciimoo/searx/master/docs/user/public_instances.rst'
 AFTER_ALIVE_AND_RUNNING = re.compile('Alive and running(.*)', re.MULTILINE | re.DOTALL)
 ITEM_RE = r'\* (.+)'
-LINK_RE = r'`([^\ ]+) <([^\>]+)>`'
+LINK_RE = r'`([^<]+)<([^>]+)>`__'
 
 
 def normalize_url(url):
     if url.startswith('https://www.ssllabs.com/') or \
-        url.startswith('https://hstspreload.org/') or \
-        url.startswith('https://geti2p.net/') or \
-        url.endswith('/cert/'):
-        return False
+       url.startswith('https://hstspreload.org/') or \
+       url.startswith('https://geti2p.net/') or \
+       url.endswith('/cert/'):
+        return None
     if url.endswith('/'):
         url = url[:-1]
     if url.endswith('/search'):
@@ -22,7 +22,7 @@ def normalize_url(url):
     # Remove .i2p (keep .onion URL)
     host = get_host(url)
     if host.endswith('.i2p'):
-        return False
+        return None
     url = url + '/'
     return url
 
@@ -33,16 +33,20 @@ async def get_instance_urls():
     # fetch the .rst source
     async with new_client() as session:
         response = await session.get(SEARX_INSTANCES_URL, timeout=10)
+    # get source after 'Alive and running'
     match = re.search(AFTER_ALIVE_AND_RUNNING, response.text)
     if match:
+        # for each item of a list
         lines = re.findall(ITEM_RE, match.group(0))
-        if len(lines) > 0:
-            for line in lines:
-                links = re.findall(LINK_RE, line)
-                for link in links:
-                    url = normalize_url(link[1])
-                    if url:
-                        instance_urls.append(url)
+        for line in lines:
+            # for each link
+            links = re.findall(LINK_RE, line)
+            for link in links:
+                # normalize the link
+                url = normalize_url(link[1])
+                if url:
+                    # add it
+                    instance_urls.append(url)
 
     # remove duplicates
     instance_urls = list(set(instance_urls))
