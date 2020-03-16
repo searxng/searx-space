2329d059b595d75179a50952efa9a88972cd97ff
Dalf <alex@al-f.net>
2020-03-16 19:49:36 +0100

    [mod] docker: use the Firefox 74 instead of firefox-esr debian package

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@ BASE_DIR="$(dirname -- "`readlink -f -- "$0"`")"
 cd -- "$BASE_DIR"
 set -e
 
+export PATH=/opt/firefox:$PATH
+
 /usr/bin/tor -f /usr/local/searxstats/torrc
 
 python3 -msearxstats $@
