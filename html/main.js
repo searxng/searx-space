664a8a5207cb81400e11e67670879cb3c6d49adc
Matthew Fleming <matthew@matteqa.com>
2020-12-30 15:18:53 +1000

    Add JavaScript license information for LibreJS

--- html/main.js
+++ html/main.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3
+
 /* eslint-disable no-undef */
 /* eslint-disable strict */
 /* eslint-disable func-names */
@@ -925,3 +927,5 @@ new Vue({
         });
     },
 });
+
+// @license-end
